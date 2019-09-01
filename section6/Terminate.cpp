c42df2b5f59889cd5ecbe52cbcf40a8cf43a601e
yuji nunome <yu.ji.d.is+github2@gmail.com>
2019-09-01 22:47:59 +0900

    loopに切り替え

--- section6/Terminate.cpp
+++ section6/Terminate.cpp
@@ -7,30 +7,24 @@
 
 // 10^6
 const uint64_t vector_num = 1000 * 1000;
-// 10^0
-const size_t loop = 1;
+// 10^1
+const size_t loop = 10;
 std::random_device seed_gen;
 auto engine = std::mt19937_64(seed_gen());
 auto int_rand = std::uniform_int_distribution<>(0, 9);
 
 void every_strlen(char *str, int &sum) {
-  FUNC_TIMER;
-  for (size_t i = 0; i < loop; ++i) {
-    for (size_t j = 0; j < std::strlen(str); ++j) {
-      if (str[j] == '5') {
-        sum += 5;
-      }
+  for (size_t j = 0; j < std::strlen(str); ++j) {
+    if (str[j] == '5') {
+      sum += 5;
     }
   }
 }
 
 void first_strlen(char *str, int &sum) {
-  FUNC_TIMER;
-  for (size_t i = 0; i < loop; ++i) {
-    for (size_t j = 0, end = std::strlen(str); j < end; ++j) {
-      if (str[j] == '5') {
-        sum += 5;
-      }
+  for (size_t j = 0, end = std::strlen(str); j < end; ++j) {
+    if (str[j] == '5') {
+      sum += 5;
     }
   }
 }
@@ -43,8 +37,9 @@ int main(void) {
 
   int sum = 0;
 
-  every_strlen(str, sum);
-  first_strlen(str, sum);
+  loop_time("every strlen", loop, every_strlen, str, sum);
+  loop_time("first strlen", loop, first_strlen, str, sum);
 
   std::cout << sum << std::endl;
+  return 0;
 }
